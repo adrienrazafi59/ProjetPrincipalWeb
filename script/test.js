01a5074f1fa16ef78150ce3472b8e66a9c61e453
adrienrazafi59 <razafi.adrien59@gmail.com>
2024-11-19 15:29:20 +0100

    Javascript

---
+++ script/test.js
@@ -0,0 +1,19 @@
+//console.log("Hello World");
+
+let userName = "DonmaiMan";
+let age = 22;
+let size = 1.80;
+let isCool = true;
+
+console.log(userName,"is",size,"meters tall and is cool =",isCool);
+
+isCool = false;
+
+console.log(userName, "is cool =" ,isCool);
+
+console.log(userName, "'s real size is", size-.05, "meters");
+
+let a = 34;
+let b = 98;
+
+console.log(a-b, a+b, a*b, a/b, a%b);
